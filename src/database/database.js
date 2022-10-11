4e50e8de6cc9a130a6bd1a6684a5ad4949a245ce
guedesclaudio <cgsarmentosilva@gmail.com>
2022-10-11 18:58:47 -0300

    FEAT : Prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -4,13 +4,22 @@ dotenv.config()
 
 const { Pool } = pg
 
+/*
 const connection = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: 'localhost',
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME
-});
+});*/
 
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URI,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(databaseConfig);
 
 export default connection
